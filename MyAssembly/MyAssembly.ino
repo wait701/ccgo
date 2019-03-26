e2af4f3a78f393d45021262280224796e3bba18e
jung seok <cmslja@naver.com>
2019-03-26 11:34:10 +0900

    dd

---
+++ MyAssembly/MyAssembly.ino
@@ -0,0 +1,61 @@
+#include <StringTok.h>
+#define SERIAL_BPS  (9600)
+#define DELAY_MS  (2000)
+
+void setup() {
+    Serial.begin(SERIAL_BPS);
+}
+
+void loop() {
+    String sInput = inputString();
+    if (sInput.length() > 0) {  // Not empty?
+    double ans = processString(sInput);
+    printString(ans);
+  }
+  delay(DELAY_MS);
+}
+
+String inputString() {
+  StringTok stInput;
+  stInput.inputSerial();
+  return stInput.toString();
+}
+
+double processString(String sInput) {
+  double ans = 0.;
+  StringTok stInput(sInput);
+  StringTok stToken1, stToken2, stToken3;
+  stToken1 = stInput.getToken();
+  stToken2 = stInput.getTokenNum();
+  stToken3 = stInput.getTokenNum();
+  if (stToken1.toString() == "add")
+    ans = funAdd(stToken2.atof(), stToken3.atof());
+  else if (stToken1.toString() == "sub")
+    ans = funSub(stToken2.atof(), stToken3.atof());
+      else if (stToken1.toString() == "mul")
+    ans = funMul(stToken2.atof(), stToken3.atof());
+      else if (stToken1.toString() == "div")
+    ans = funDiv(stToken2.atof(), stToken3.atof());
+  
+  return ans;
+}
+
+void printString(double ans) {
+  Serial.print("답 = "); Serial.println(ans);
+}
+
+double funAdd(double a, double b) {
+  return a + b;
+}
+
+double funSub(double a, double b) {
+  return a - b;
+}
+
+double funMul(double a, double b) {
+  return a * b;
+}
+
+double funDiv(double a, double b) {
+  return a / b;
+}
